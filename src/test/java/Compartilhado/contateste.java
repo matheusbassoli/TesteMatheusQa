52f9d8a396753d740671487dbd9114d84bf201e6
Matheus Albertini Bassoli <matheus.bassoli@spcbr.int.br>
2018-10-01 21:51:31 -0300

    Teste Matheus

--- src/test/java/Compartilhado/contateste.java
+++ src/test/java/Compartilhado/contateste.java
@@ -6,7 +6,8 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/TesteCucumber/TesteMatheus.feature", tags = "@TesteQA", 
+@CucumberOptions(features = "src/test/resources/TesteCucumber/TesteMatheus.feature", tags = "@TesteQA",
+plugin = {"json:/var/lib/jenkins/jobs/teste_PreProd/workspace/RoteiroTesteSPCBrasil_Vinicius/target/cucumber-reports/1.json", "html:/var/lib/jenkins/jobs/teste_PreProd/workspace/RoteiroTesteSPCBrasil_Vinicius/target/cucumber-reports/1", "rerun:/var/lib/jenkins/jobs/teste_PreProd/workspace/RoteiroTesteSPCBrasil_Vinicius/target/cucumber-reports/1.txt", "pretty"},
 glue = "passos.testes", monochrome = true, dryRun = false)
 public class contateste {
 }
